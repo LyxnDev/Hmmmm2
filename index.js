d064bbef1e7cef1be9efca609a70f465e2698dd4
LyxnDev <168339425+LyxnDev@users.noreply.github.com>
2025-04-23 12:36:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const { exec } = require('child_process');
 
 // Ganti dengan token bot Telegram Anda
-const token = '7069670027:AAGrM4SX8wAyoFmZpKQQo83qS3aANewmleI';
+const token = '7729532380:AAHQgIrbq7r6T23hhVU0zuE8UmsiEls6x-E';
 
 // Inisialisasi bot dengan token
 const bot = new TelegramBot(token, { polling: true });
